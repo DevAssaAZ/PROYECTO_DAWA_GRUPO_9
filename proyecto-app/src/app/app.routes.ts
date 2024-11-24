a35f9a9c67598503939849d02d45b49a2d535ef1
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-24 10:23:20 -0500

    routeo link garantias

--- proyecto-app/src/app/app.routes.ts
+++ proyecto-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CrudGarantiasComponent } from './components/crud-garantias/crud-garantias.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"crud-garantias", component:CrudGarantiasComponent}
+
+];
--- proyecto-app/src/app/components/crud-garantias/crud-garantias.component.ts
+++ proyecto-app/src/app/components/crud-garantias/crud-garantias.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './crud-garantias.component.css'
 })
 export class CrudGarantiasComponent {
-
+  title="Crud de Garantias";
 }
