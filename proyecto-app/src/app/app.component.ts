8b771169e3ecdd95f150c9b89e4eef2110ae7b06
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-24 09:44:04 -0500

    Avance NavBar

--- proyecto-app/src/app/app.component.ts
+++ proyecto-app/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto-app';
+  title = 'Portal de Asistencia Postventa - Postventa360';
 }
