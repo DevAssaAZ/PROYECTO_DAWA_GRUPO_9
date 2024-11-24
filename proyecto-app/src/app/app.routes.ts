06ca1aec11dd8274d6ae7ea1b1ab971d79c530ae
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-24 10:37:59 -0500

    Update app.routes.ts

--- proyecto-app/src/app/app.routes.ts
+++ proyecto-app/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { CrudGarantiasComponent } from './components/crud-garantias/crud-garanti
 
 export const routes: Routes = [
 
-    {path:"crud-garantias", component:CrudGarantiasComponent}
+    {path:"crud-garantias", component:CrudGarantiasComponent},
 
+     //rutas por defecto
+     { path: '', redirectTo: '/crud-garantias', pathMatch: 'full' },
+     {path:"**", redirectTo:"crud-garantias"}, //**  Representa cualquier otra ruta
 ];
