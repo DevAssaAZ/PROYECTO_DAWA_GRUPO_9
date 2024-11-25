4382100eb2537008657a71b354cc1fe44b2244ed
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-25 18:31:38 -0500

    navbar

--- proyecto-app/src/app/app.routes.ts
+++ proyecto-app/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
 import { CrudGarantiasComponent } from './components/crud-garantias/crud-garantias.component';
 import { CrudDevolucionesComponent } from './components/crud-devoluciones/crud-devoluciones.component';
+import { IndexComponent } from './components/index/index.component';
+import { CrudClientesComponent } from './components/crud-clientes/crud-clientes.component';
 
 export const routes: Routes = [
 
     {path:"crud-garantias", component:CrudGarantiasComponent},
     {path: "crud-devoluciones", component:CrudDevolucionesComponent},
+    {path: "crud-clientes", component:CrudClientesComponent},
+    {path: "inicio", component:IndexComponent},
 
      //rutas por defecto
-     { path: '', redirectTo: '/crud-garantias', pathMatch: 'full' },
-     {path:"**", redirectTo:"/crud-garantias"}, //**  Representa cualquier otra ruta
+     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+     {path:"**", redirectTo:"/inicio"}, //**  Representa cualquier otra ruta
 ];
