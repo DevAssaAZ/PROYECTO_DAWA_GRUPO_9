c81f14175e40574a686be0641c47324ff8b8663d
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-25 05:54:27 -0500

    avance garantias y dialog

--- proyecto-app/src/app/components/crud-garantias/crud-garantias.component.ts
+++ proyecto-app/src/app/components/crud-garantias/crud-garantias.component.ts
@@ -1,12 +1,194 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { DatePipe } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Garantia } from '../../models/Garantia';
+import { GarantiasjsonService } from '../../services/garantiasjson.service';
+import { DialogComponent } from '../../shared/dialog/dialog.component';
 
 @Component({
   selector: 'app-crud-garantias',
   standalone: true,
-  imports: [],
+  imports: [MatFormField, MatLabel, MatButtonModule, MatInputModule, MatTableModule, MatPaginatorModule, DatePipe,
+    MatRadioModule, MatSelectModule, MatCheckboxModule, MatDatepickerModule, MatOptionModule, MatFormFieldModule,
+    MatNativeDateModule, ReactiveFormsModule],
   templateUrl: './crud-garantias.component.html',
   styleUrl: './crud-garantias.component.css'
 })
-export class CrudGarantiasComponent {
-  title="Crud de Garantias";
+export class CrudGarantiasComponent implements OnInit, AfterViewInit{
+  form! :FormGroup;
+  isEditMode:boolean = false;
+  currentId! :number;
+
+  //DataSource(fuente de datos) para mi tabla
+  dataSource = new MatTableDataSource<Garantia>();
+  @ViewChild(MatPaginator) paginator!:MatPaginator;
+  ngAfterViewInit():void{
+    this.dataSource.paginator = this.paginator;
+  }
+  
+
+  ngOnInit():void{
+  this.getGarantias();
+  //Inicializar las variables asociadas a los componentes de los formularios
+  this.form= this.fb.group({
+    cliente_id:["", [Validators.required]],
+    producto: ["", [Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z0-9 ]+$/)]],
+    numeroFactura:["",[Validators.required]],
+    fechaCompra: ["",[Validators.required]],
+    descripcion: ["",[Validators.required]],
+    estado: "Pendiente",
+    fechaRegistro: ["",[Validators.required]],
+    ultimaActualizacion: [""],
+  });
+  }
+
+  
+  constructor(private garantiaService:GarantiasjsonService, private fb: FormBuilder, private mydialog: MatDialog){
+  }
+
+  getGarantias(): void{
+    this.garantiaService.getGarantias().subscribe((datos:Garantia[])=>{
+      this.dataSource.data = datos;
+    });
+  }
+
+  search(searchInput:HTMLInputElement){
+    if(searchInput.value){//search.value es lo que el usuario escribio en la caja de texto
+      //buscar
+      this.garantiaService.getGarantiaSearch(searchInput.value).subscribe((datos:Garantia[])=>{
+        this.dataSource.data = datos;
+      });
+
+    }else{// listar todas las peliculas
+      this.getGarantias();
+    }
+  }
+
+  eliminar(garantia:Garantia){
+    const dialogRef = this.mydialog.open(DialogComponent,{
+      data:{
+        titulo:"Eliminar " ,
+        contenido: 'Desea eliminar este registro de garantia' +  " ?"
+      }
+    }); //Abrir la ventana
+    dialogRef.afterClosed().subscribe(result=>{
+      if(result==="Aceptar"){
+        this.garantiaService.deleteGarantia(garantia).subscribe(()=>{
+          alert("Eliminado exitosamente");
+          this.getGarantias(); //actualizar el dataSource
+        });
+      }else if(result==="Cancelar"){
+        this.getGarantias();
+      }
+    })
+
+
+    
+
+  }
+
+  editar(garantia:Garantia){
+
+    this.isEditMode = true;
+
+    if(garantia && garantia.id){
+      this.currentId = garantia.id;
+    }else{
+      console.log("Garantia o el Id de la garantia estan undefined");
+    }
+
+    //Cargar los datos de la pelicula en los controles del formulario
+    this.form.setValue({
+      cliente_id: garantia.cliente_id,
+      producto: garantia.producto,
+      numeroFactura:garantia.numeroFactura,
+      fechaCompra: garantia.fechaCompra,
+      descripcion: garantia.descripcion,
+      estado: garantia.estado,
+      fechaRegistro: garantia.fechaRegistro,
+      ultimaActualizacion: garantia.ultimaActualizacion,
+    });
+  }
+
+  onSubmit(){
+    //es guardar la garantia
+
+    //revisar si el formulario es valido
+    if(this.form.invalid){
+      return;
+    }
+
+    //obtener los datos de los controles del formulario
+    const newGarantia: Garantia= this.form.value;
+
+    if(this.isEditMode){//editar
+
+      newGarantia.id = this.currentId;
+
+      const dialogRef = this.mydialog.open(DialogComponent,{
+        data:{
+          titulo:"Editar ",
+          contenido: 'Desea guardar la modificacion de esta garantia ' + " ?"
+        }
+      }); //Abrir la ventana
+      dialogRef.afterClosed().subscribe(result=>{
+        if(result==="Aceptar"){
+          this.garantiaService.updateGarantia(newGarantia).subscribe((updateGarantia)=>{
+            alert("Garantia editada exitosamente");
+            this.clearForm();
+            this.getGarantias(); // actualizar el datasource de la table de garantias
+          });
+        }else if(result==="Cancelar"){
+          this.getGarantias();
+        }
+      })
+      
+    }else{//agregar
+
+      const dialogRef = this.mydialog.open(DialogComponent,{
+        data:{
+          titulo:"Agregar Nueva Garantia" ,
+          contenido: 'Desea guardar esta nueva Garantia? ' + " ?"
+        }
+      }); //Abrir la ventana
+      dialogRef.afterClosed().subscribe(result=>{
+        if(result==="Aceptar"){
+          this.garantiaService.addGarantia(newGarantia).subscribe((addGarantia)=>{
+            alert("Garantia agregada exitosamente");
+            this.clearForm();
+            this.getGarantias(); // actualizar el datasource de la table de garantias
+          });
+        }else if(result==="Cancelar"){
+          this.getGarantias();
+        }
+      })
+    }
+    
+  }
+
+  clearForm():void{
+    this.form.reset({
+      cliente_id: '',
+      producto: '',
+      numeroFactura: '',
+      fechaCompra: '',
+      descripcion: '',
+      estado: '',
+      fechaRegistro: '',
+      ultimaActualizacion: '',
+    });
+    this.currentId = 0;
+    this.isEditMode = false;
+  }
 }
