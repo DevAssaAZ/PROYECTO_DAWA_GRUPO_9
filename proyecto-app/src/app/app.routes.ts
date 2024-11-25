9d3cea37fc18b017ad18bf0d9bd5f3fbc49444e0
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-25 16:12:02 -0500

    router de devoluciones

--- proyecto-app/src/app/app.routes.ts
+++ proyecto-app/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { CrudGarantiasComponent } from './components/crud-garantias/crud-garantias.component';
+import { CrudDevolucionesComponent } from './components/crud-devoluciones/crud-devoluciones.component';
 
 export const routes: Routes = [
 
     {path:"crud-garantias", component:CrudGarantiasComponent},
+    {path: "crud-devoluciones", component:CrudDevolucionesComponent},
 
      //rutas por defecto
      { path: '', redirectTo: '/crud-garantias', pathMatch: 'full' },
-     {path:"**", redirectTo:"crud-garantias"}, //**  Representa cualquier otra ruta
+     {path:"**", redirectTo:"/crud-garantias"}, //**  Representa cualquier otra ruta
 ];
