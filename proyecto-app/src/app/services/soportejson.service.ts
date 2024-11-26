a4134e2004bc1017ad9e85dd59a5947e38406940
danielfrancoemp <afrancoemperador@gmail.com>
2024-11-26 13:07:37 -0500

    Update soportejson.service.ts

--- proyecto-app/src/app/services/soportejson.service.ts
+++ proyecto-app/src/app/services/soportejson.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Soporte } from '../models/SolicitudesSoporte';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoportejsonService {
+  private jsonUrl = 'http://localhost:3000/soporte'; // Ruta del JSON
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // OBTENER TODOS LOS REGISTROS
+  getSoportes(): Observable<Soporte[]> {
+    return this.http.get<Soporte[]>(this.jsonUrl);
+  }
+
+  // BUSCAR REGISTROS (por cliente_id, estado, o ambos)
+  getSoporteSearch(cliente_id?: string, estado?: string): Observable<Soporte[]> {
+    return this.http.get<Soporte[]>(this.jsonUrl).pipe(
+      map((soportes) =>
+        soportes.filter((soporte) =>
+          (cliente_id ? soporte.cliente_id.toLowerCase().includes(cliente_id.toLowerCase()) : true) &&
+          (estado ? soporte.estado.toLowerCase().includes(estado.toLowerCase()) : true)
+        )
+      )
+    );
+  }
+
+  // CREAR UN NUEVO REGISTRO
+  addSoporte(soporte: Soporte): Observable<Soporte> {
+    return this.http.post<Soporte>(this.jsonUrl, soporte);
+  }
+
+  // MODIFICAR UN REGISTRO EXISTENTE
+  updateSoporte(soporte: Soporte): Observable<Soporte> {
+    const urlDelSoporte = `${this.jsonUrl}/${soporte.id}`;
+    return this.http.put<Soporte>(urlDelSoporte, soporte);
+  }
+
+  // ELIMINAR UN REGISTRO
+  deleteSoporte(soporte: Soporte): Observable<void> {
+    const urlDelSoporte = `${this.jsonUrl}/${soporte.id}`;
+    return this.http.delete<void>(urlDelSoporte);
+  }
 }
