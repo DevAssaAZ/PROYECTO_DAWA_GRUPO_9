f40bc34ab748f2ef75bc80c9e5ab5243ab810dea
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-26 09:06:40 -0500

    footer

--- proyecto-app/src/app/components/index/index.component.ts
+++ proyecto-app/src/app/components/index/index.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
