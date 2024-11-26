0e6d4d863c4106fc917a2981ac146a78c789fec2
britanydb <duqueborborbritany@gmail.com>
2024-11-25 22:42:25 -0500

    componente crud clientes

--- proyecto-app/src/app/components/crud-clientes/crud-clientes.component.ts
+++ proyecto-app/src/app/components/crud-clientes/crud-clientes.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { Clientes } from '../../models/Clientes';
+import { MatPaginator } from '@angular/material/paginator';
+import { ClientesjsonService } from '../../services/clientesjson.service';
 
 @Component({
   selector: 'app-crud-clientes',
@@ -7,6 +12,23 @@ import { Component } from '@angular/core';
   templateUrl: './crud-clientes.component.html',
   styleUrl: './crud-clientes.component.css'
 })
-export class CrudClientesComponent {
-
+export class CrudClientesComponent implements OnInit, AfterViewInit{
+  form!:FormGroup;
+  isEditMode: boolean = false;
+  currentId!:number;
+  dataSource = new MatTableDataSource<Clientes>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  ngOnInit(): void {
+    this.getClientes();
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator= this.paginator;
+  }
+  constructor(private clienteService:ClientesjsonService, private fb: FormBuilder){
+  }
+  getClientes():void{
+    this.clienteService.getClientes().subscribe((datos:Clientes[])=>{
+      this.dataSource.data = datos;
+    });
+  }
 }
