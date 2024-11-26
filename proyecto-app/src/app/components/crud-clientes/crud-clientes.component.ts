30d6763351923b636c8286af8b868a7c6536868f
britanydb <duqueborborbritany@gmail.com>
2024-11-26 11:47:05 -0500

    formulario

--- proyecto-app/src/app/components/crud-clientes/crud-clientes.component.ts
+++ proyecto-app/src/app/components/crud-clientes/crud-clientes.component.ts
@@ -1,18 +1,24 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Clientes } from '../../models/Clientes';
 import { MatPaginator } from '@angular/material/paginator';
 import { ClientesjsonService } from '../../services/clientesjson.service';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { DatePipe } from '@angular/common';
+import { MatRadioButton } from '@angular/material/radio';
+import { MatSelect } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-crud-clientes',
   standalone: true,
-  imports: [MatFormField, MatLabel, MatButtonModule, MatInputModule, MatTableModule, MatPaginator, DatePipe],
+  imports: [MatFormField, MatLabel, MatButtonModule, MatInputModule, MatTableModule, MatPaginator, DatePipe,
+    MatCheckboxModule, MatDatepickerModule, MatOptionModule, MatFormFieldModule, ReactiveFormsModule, MatNativeDateModule],
   templateUrl: './crud-clientes.component.html',
   styleUrl: './crud-clientes.component.css'
 })
@@ -45,4 +51,8 @@ export class CrudClientesComponent implements OnInit, AfterViewInit{
       this.getClientes();
     }
   }
+  onSubmit(){
+
+  }
+
 }
