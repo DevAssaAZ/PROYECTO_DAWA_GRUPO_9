9f231d901d2f61c37399c96a03bc13b38935694b
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2024-11-26 08:28:26 -0500

    Cambios en navbar

--- proyecto-app/src/app/app.component.ts
+++ proyecto-app/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CrudClientesComponent } from './components/crud-clientes/crud-clientes.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
