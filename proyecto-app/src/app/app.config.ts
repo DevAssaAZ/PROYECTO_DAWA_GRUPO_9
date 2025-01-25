c0e22786b1e0e4b6076f590566ddd3a993a642b3
Peñafiel Del Rosario Gabriel <gabriel.penafieldel@ug.edu.ec>
2025-01-24 19:11:48 -0500

    ee

--- proyecto-app/src/app/app.config.ts
+++ proyecto-app/src/app/app.config.ts
@@ -6,6 +6,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync(), provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideAnimationsAsync(), provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
 
